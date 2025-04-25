e16a18bc7231c909eee7ac8828bd517a85b541b4
sohamsabane <sohamsabane48@gmail.com>
2025-04-25 19:34:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+var typed = new Typed(".text", {
+    strings: ["Machine Learning" , "UI/UX design" , "Web development"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
